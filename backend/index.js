4532050df79b68c49fe329e8b8232a712bf7cc5f
DanielFelix-eng <felixjardini502@gmail.com>
2026-06-26 19:47:57 +0300

    Prepare frontend for deployment

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,9 @@ import cors from 'cors'
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
-app.use(cors({origin: 'http://localhost:5173', credentials: true}))
+app.use(cors({origin: 'http://localhost:5173', 
+   https://jardini-homes.onrender.com ,
+   credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 app.get('/', (req, res) => {
