ec61de5719b5dca30b459be3d37c9793de80d2f5
DanielFelix-eng <felixjardini502@gmail.com>
2026-06-26 21:03:12 +0300

    add pre installation

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,10 @@ import dotenv from 'dotenv'
 import authRoute from './routes/authroutes.js' 
 import PropertyRoutes from './routes/propertyroutes.js'
 import cors from 'cors'
+import path from 'path'
 
-dotenv.config()
+dotenv.config() 
+const __dirname =  path.resolve(); 
 const app = express()
 const PORT = process.env.PORT || 3000
 app.use(cors({ 
@@ -21,6 +23,15 @@ app.get('/', (req, res) => {
 })
 app.use('/api/auth', authRoute)
 app.use('/api/properties', PropertyRoutes)
+app.use(express.static(path.join(__dirname , '/frontend/build')))
+app.get('*' , (req,res) => {
+   res.sendFile(path.join(__dirname , 'frontend' , 'dist' , 'index.html'))
+ })
+ app.use( (err,req,res)=>{
+   const statusCode = err.statusCode || 500 ; 
+   const message =  err.message || 'Internal Server'
+ }) 
+ 
 app.listen(PORT, () => {
   conectDB()
   console.log('server is running on port ' + PORT)
