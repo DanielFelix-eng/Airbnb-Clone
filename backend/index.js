a1cb75d81a742cddd3c60d8a18d119b837f21190
DanielFelix-eng <felixjardini502@gmail.com>
2026-06-26 22:55:05 +0300

    index changes

--- backend/index.js
+++ backend/index.js
@@ -23,9 +23,9 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoute)
 app.use('/api/properties', PropertyRoutes)
 app.use(express.static(path.join(__dirname , '/frontend/build')))
-app.get('*' , (req,res) => {
-   res.sendFile(path.join(__dirname , 'frontend' , 'dist' , 'index.html'))
- })
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/dist/index.html"));
+});
  app.use( (err,req,res)=>{
    const statusCode = err.statusCode || 500 ; 
    const message =  err.message || 'Internal Server'
